1e5673d4bde727bbc006254ef884af80088eb2e0
anand <anandzzz1973@gmail.com>
2020-05-16 13:10:36 +0530

    this is simple coin toss using js

---
+++ script.js
@@ -0,0 +1,13 @@
+
+document.querySelector('.coin').style.display='none'
+document.querySelector('#coin1').addEventListener('click',function()
+{
+	var hell=Math.floor(Math.random()*2)+1;
+	console.log(hell);
+
+	document.querySelector('.coin').style.display='block';
+    //console.log(hell);
+    
+    document.querySelector('.coin').src='coin-'+hell+'.jpg'; 
+
+})
